c7127d4d45ec423f2f1375f93d9334298bc8ed53
jburns5027 <jburns5027@fullsail.edu>
2012-05-14 18:32:28 -0400

    More jqmobile

--- Burns_Joseph_Project2/Gold App/JQMobile Template/js/main.js
+++ Burns_Joseph_Project2/Gold App/JQMobile Template/js/main.js
@@ -1,1 +1,20 @@
 // write your javascript in here
+parsewOname=function(data){
+// Uses form Data here
+
+};
+$(document).ready(function(){
+
+var wOname = $("#workoutname");
+
+wOname.validate({
+
+invalidHandler: function(form, validator){},
+submitHandler: function(){
+			data = wOname.seralizeArray();
+			parsewOnameForm(data);
+			
+			}
+		});
+
+}); 
