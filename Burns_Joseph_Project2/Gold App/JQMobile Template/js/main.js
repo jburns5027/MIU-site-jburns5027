dcc6ebd3254219f929529394cbcf0c005788cc7c
jburns5027 <jburns5027@fullsail.edu>
2012-05-15 20:58:00 -0400

    Trying to add dialog box.

--- Burns_Joseph_Project2/Gold App/JQMobile Template/js/main.js
+++ Burns_Joseph_Project2/Gold App/JQMobile Template/js/main.js
@@ -5,12 +5,26 @@ parsewOname=function(data){
 };
 $(document).ready(function(){
 
-var wOname = $("#workoutname");
+var wOname = $("#workoutname"),
+	rErrorslink= $("#rErrorslink")
+
+;
 
 wOname.validate({
 
-invalidHandler: function(form, validator){},
-submitHandler: function(){
+invalidHandler: function(form, validator){
+		rErrorslink.click();
+		var html = '';
+		for(var key in validator.submitted){
+				var label = $('label[for^="''+ key +"]').not('[generated]');
+				var legend = label.closest('fieldset').find('.ui-controlgroup-label')
+				var fieldName = legend.length ? legend.text : label.text();
+				html += '<li>' + fieldname + '</li>';
+		};
+		$("#runningErrors ul").html(html);
+
+},
+submitHandler: function(){ 
 			data = wOname.seralizeArray();
 			parsewOnameForm(data);
 			
